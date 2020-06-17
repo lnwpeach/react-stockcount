6ba4f6f898ff669ba4793af2a9129f4f33626dad
Peach <pichayuth.c@gmail.com>
2020-06-17 09:18:56 +0700

    config

--- stockcount-api/config.php
+++ stockcount-api/config.php
@@ -4,7 +4,7 @@ $host       = 'localhost';
 $db_type    = 'mysql';
 $db         = 'stockcount';
 $username   = 'root';
-$password   = '12345878';
+$password   = '';
 
 $pdo = new PDO("{$db_type}:host={$host};dbname={$db};charset=utf8", $username, $password);
 ?>
